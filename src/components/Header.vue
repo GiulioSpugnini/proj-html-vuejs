7c92bec91ad6289153d04229e934932472129466
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-19 16:41:51 +0100

    header finished

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,14 @@
 <template>
   <header class="container d-flex justify-content-between align-items-center">
-    <div class="col-4">
+    <div class="col-4 text-center">
       <img src="../assets/img/logo.png" alt="" />
     </div>
     <div class="col-8">
-      <ul class="d-flex justify-content-evenly">
+      <ul class="d-flex justify-content-evenly align-items-center m-0 p-0">
         <li v-for="(navItem, index) in navItems" :key="index">
           <figure class="text-center">
-            <img class="nav-icons" :src=" require(`../assets/img/${navItem.toLowerCase()}.png`) " alt="">
-            <figcaption>{{ navItem }}</figcaption>
+            <img :src=" require(`../assets/img/${navItem.toLowerCase()}.png`) " alt="">
+            <figcaption class="fs-5 my-3">{{ navItem }}</figcaption>
           </figure>
         </li>
       </ul>
@@ -24,7 +24,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.nav-icons{
-  
+@import '../assets/scss/style.scss';
+figcaption{
+  color: $navbar-color;
 }
 </style>
