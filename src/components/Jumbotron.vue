d81999b38302fa969ed6e058c91a843b366e4d30
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-20 16:07:57 +0100

    ended jumbotron

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="position-relative" :style="{ backgroundImage: 'url(' + jumboBg + ')' }">
+    <img id="car" class="position-absolute" src="../assets/img/slider_slide3_img3.png" alt="" />
+    <img id="breaks" class="position-absolute" src="../assets/img/slider_slide3_img2.png" alt="" />
+    <div id="jumboFooter" class="position-absolute w-100" :style="{ backgroundImage: 'url(' + jumboFooterBg + ')' }">
+        <div class="container d-flex justify-content-between">
+            <h2 class="my-5 fs-1 d-inline text-light">How to Enroll Your Child to a Class?</h2>
+            <button class="px-3 m-5">LEARN MORE</button>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "jumbotron",
+  data() {
+    return {
+      jumboBg: require("../assets/img/slider_slide3_background.png"),
+      jumboFooterBg: require("../assets/img/pattern.png"),
+    };
+  },
+};
+</script>
+
+<style>
+section {
+  min-height: 600px;
+}
+#car{
+ top: 70%;
+ left: 70%;
+}
+#breaks{
+    top: 60%;
+ left: 40%;
+}
+#jumboFooter{
+    top: 100%;
+    left: 0;
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,16 @@
 <template>
-  <div></div>
+  <main>
+      <Jumbotron/>
+  </main>
 </template>
 
 <script>
+import Jumbotron from './Jumbotron.vue';
 export default {
 name: 'Main',
+components:{
+    Jumbotron,
+}
 }
 </script>
 
